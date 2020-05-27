57c6fdc99c20604e232aac8ed7224e4740e382b6
jsi-webdev <j.skullow@gmail.com>
2020-05-28 04:56:01 +0900

    Added gamePattern algorithm

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-$("h1");
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(3 * Math.random()) + 1;
+  return randomNumber;
+}
+
+var randomChosenColor = buttonColors[nextSequence()];
+gamePattern.push(randomChosenColor);
